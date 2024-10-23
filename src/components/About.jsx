b432cabc44f67e3c488f35cbb331e1f3c918ff3e
Adam Dasovich <adamdasovich@gmail.com>
2024-10-23 14:57:36 -0400

    changed the About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+    <h1>Louis Slutton will be the most interactive and exciting site.</h1>
+    <div>This site will be up by October 31, 2024</div>
+    </div>
   )
 }
 
